9ed7d8c259de023e62522e8f7d871e6a93f70cac
Animesh Shaw <PsychoCoderHC@users.noreply.github.com>
2014-02-22 02:20:10 +0530

    Update HashIdentifier.py
    Code Updated. Added 38 New Hashes.
    
    New Version 3.2
    
    Minecraft(AuthMe)
    Keccak-512
    SHA-1(Hex)
    MD5 crypt
    MD5 apache crypt
    SHA1(LADP) crypt
    SHA1(LADP) salt + Base64
    Blowfish crypt
    NT crypt
    SHA-1 crypt
    SHA-256 crypt
    SHA-512 crypt
    Fortigate(FortiOS)
    Apache MD5
    FCS-32
    ELF-32
    Fletcher-32
    FreeBSD MD5
    Cisco-IOS Md5
    MD5(ZipMonster)
    DES crypt
    DES hash(Traditional)
    CRC-64
    substr(md5($pass),0,16)
    substr(md5($pass),16,16)
    substr(md5($pass),8,16)
    Cisco IOS SHA26
    MD5(Chap)
    MSCASH2
    and Several Others

--- HashIdentifier.py
+++ HashIdentifier.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-#encoding: utf-8
-#@author: Psycho_Coder <https://psychocoder.github.io/>                            
+# encoding: utf-8
+# @author: Psycho_Coder <https://psychocoder.github.io/>                            
 
-import re,sys
+import re, sys
 
 TITLE = '''
  _     _            _        _____    _                  _  ___ _             
@@ -13,7 +13,7 @@ TITLE = '''
 |_|   |_|\_||_(___/|_| |_|  (_____)____|\____)_| |_|\___)_|_|  |_|\____)_|    
 _______________________________________________________________________________
 
-						   Version: 2.2 by Psycho_Coder
+						   Version: 3.2 by Psycho_Coder
 _______________________________________________________________________________
 	'''
 
@@ -25,45 +25,61 @@ USAGE = '''	Usage:
 	'''
 
 HASHES = (
-		("Blowfish(Eggdrop)",	"^\+[a-z0-9\/\.]{12}$"),
-		("Blowfish(OpenBSD)",	"^\$2a\$[0-9]{0,2}?\$[a-z0-9\/\.]{53}$"), 	
-		("DES(Unix)",		"^.{0,2}[a-z0-9\/\.]{11}$"),
-		("MD5(Unix)",		"^\$1\$.{0,8}\$[a-z0-9\/\.]{22}$"),
-		("MD5(APR)",		"^\$apr1\$.{0,8}\$[a-z0-9\/\.]{22}$"),
-		("MD5(MyBB)",		"^[a-f-0-9]{32}:[a-z0-9]{8}$"),
-		("MD5(Joomla)",		"^[a-f0-9]{32}:[a-z0-9]{16,32}$"),
-		("MD5(Wordpress)",	"^\$P\$[a-z0-9\/\.]{31}$"),
-		("MD5(phpBB3)",		"^\$H\$[A-Fa-z0-9\/\.]{31}$"),
-		("MD5(Cisco PIX)",	"^[a-z0-9\/\.]{16}$"),
-		("MD5(osCommerce)",	"^[a-f0-9]{32}:[a-z0-9]{2}$"),
-		("MD5(Palshop)",	"^[a-f0-9]{51}$"),
-		("MD5(IP.Board)",	"^[a-f0-9]{32}:.{5}$"),
-		("Lotus Domino",	"^\(?[a-z0-9\+\/]{20}\)?$"),
-		("Lineage II C4",	"^0x[a-f0-9]{32}$"),
-		("CRC-96(ZIP)",		"^[a-f0-9]{24}$"),
-		("Skein-1024",		"^[a-f0-9]{256}$"),
-		(("RIPEMD-320","RIPEMD-320(HMAC)"),		"^[a-f0-9]{80}$"),
-		("SHA-1(Django)",	"^sha1\$.{0,32}\$[a-f0-9]{40}$"),
-		("SHA-512(Drupal)",	"^\$S\$[a-z0-9\/\.]{52}$"),
-		("SHA-256(Django)",	"^sha256\$.{0,32}\$[a-f0-9]{64}$"),
-		("SHA-384(Django)",	"^sha384\$.{0,32}\$[a-f0-9]{96}$"),
-		("SHA-256(Unix)",	"^\$5\$.{0,22}\$[a-z0-9\/\.]{43,69}$"),
-		("SHA-512(Unix)",	"^\$6\$.{0,22}\$[a-z0-9\/\.]{86}$"),
-		(("SHA-384","SHA3-384","Skein-512(384)","Skein-1024(384)"),		"^[a-f0-9]{96}$"),
-		(("SHA-512","SHA3-512","Whirlpool","SALSA-10","SALSA-20","Skein-512","Skein-1024(512)"),	"^[a-f0-9]{128}$"),
-		("SSHA-1",		"^({SSHA})?[a-z0-9\+\/]{32,38}?(==)?$"),
-		("MySQL 5.x",		"^\*[a-f0-9]{40}$"),
-		(("MySQL 3.x","DES(Oracle)","LM","VNC","FNV-164"), 	"^[a-f0-9]{16}$"),
-		("SAM(LM_Hash:NT_Hash)","^[a-f-0-9]{32}:[a-f-0-9]{32}$"),
-		("MSSQL(2000)",		"^0x0100[a-f0-9]{0,8}?[a-f0-9]{80}$"),
-		(("MSSQL(2005)","MSSQL(2008)"),		"^0x0100[a-f0-9]{0,8}?[a-f0-9]{40}$"),				
-		(("MySQL 4.x","SHA-1","HAVAL-160","SHA-1(MaNGOS)","SHA-1(MaNGOS2)","TIGER-160","RIPEMD-160","RIPEMD-160(HMAC)","TIGER-160(HMAC)","Skein-256(160)","Skein-512(160)"),	"^[a-f0-9]{40}$"),
-		(("SHA-256","GOST R 34.11-94","RIPEMD-256","HAVAL-256","Snefru-256","RIPEMD-256(HMAC)","Keccak-256","Skein-256","Skein-512(256)"),		"^[a-f0-9]{64}$"),
-		(("HAVAL-192","Tiger-192","TIGER-192(HMAC)"),		"^[a-f0-9]{48}$"),
-		(("SHA-224","SHA-224(HMAC)","HAVAL-224","Keccak-224", "Skein-256(224)", "Skein-512(224)"),		"^[a-f0-9]{56}$"),
-		(("Adler32","FNV-32","Joaat","CRC-32","CRC-32B","GHash-32-3","GHash-32-5","XOR-32"),		"^[a-f0-9]{8}$"),
-		(("CRC-16-CCITT","CRC-16","FCS-16"),		"^[a-f0-9]{4}$"),
-		(("MD5(HMAC(Wordpress))","MD5(HMAC)","MD5","RIPEMD-128","RIPEMD-128(HMAC)","Tiger-128","Tiger-128(HMAC)","RAdmin v2.x","NTLM","Domain Cached Credentials(DCC)","Domain Cached Credentials 2(DCC2)","MD4","MD2","Snefru-128","Snefru-128(HMAC)","HAVAL-128","HAVAL-128(HMAC)","Skein-256(128)", "Skein-512(128)"),		"^[0-9a-f]{32}$"),	
+		("Blowfish(Eggdrop)", 		"^\+[a-zA-Z0-9\/\.]{12}$"),
+		("Blowfish(OpenBSD)", 		"^\$2a\$[0-9]{0,2}?\$[a-zA-Z0-9\/\.]{53}$"),
+		("Blowfish crypt",		"^\$2[axy]{0,1}\$[a-zA-Z0-9./]{8}\$[a-zA-Z0-9./]{1,}$"), 	
+		(("DES(Unix)","DES crypt","DES hash(Traditional)"), 		"^.{0,2}[a-zA-Z0-9\/\.]{11}$"),
+		("MD5(Unix)", 			"^\$1\$.{0,8}\$[a-zA-Z0-9\/\.]{22}$"),
+		(("MD5(APR)","Apache MD5"), 	"^\$apr1\$.{0,8}\$[a-zA-Z0-9\/\.]{22}$"),
+		("MD5(MyBB)", 			"^[a-fA-F0-9]{32}:[a-z0-9]{8}$"),
+		("MD5(ZipMonster)",		"^[a-fA-F0-9]{32}$"),
+		(("MD5 crypt","FreeBSD MD5","Cisco-IOS MD5"),	"^\$1\$[a-zA-Z0-9./]{8}\$[a-zA-Z0-9./]{1,}$"),
+		("MD5 apache crypt",	"^\$apr1\$[a-zA-Z0-9./]{8}\$[a-zA-Z0-9./]{1,}$"),
+		("MD5(Joomla)", 	"^[a-fA-F0-9]{32}:[a-zA-Z0-9]{16,32}$"),
+		("MD5(Wordpress)", 	"^\$P\$[a-zA-Z0-9\/\.]{31}$"),
+		("MD5(phpBB3)", 	"^\$H\$[a-zA-Z0-9\/\.]{31}$"),
+		("MD5(Cisco PIX)", 	"^[a-zA-Z0-9\/\.]{16}$"),
+		("MD5(osCommerce)", 	"^[a-fA-F0-9]{32}:[a-zA-Z0-9]{2}$"),
+		("MD5(Palshop)", 	"^[a-fA-F0-9]{51}$"),
+		("MD5(IP.Board)", 	"^[a-fA-F0-9]{32}:.{5}$"),
+		("MD5(Chap)",		"^[a-fA-F0-9]{32}:[0-9]{32}:[a-fA-F0-9]{2}$"),
+		("Fortigate (FortiOS)",	"^[a-fA-F0-9]{47}$"),
+		("Minecraft(Authme)",	"^\$sha\$[a-zA-Z0-9]{0,16}\$[a-fA-F0-9]{64}$"),
+		("Lotus Domino", 	"^\(?[a-zA-Z0-9\+\/]{20}\)?$"),
+		("Lineage II C4", 	"^0x[a-fA-F0-9]{32}$"),
+		("CRC-96(ZIP)", 	"^[a-fA-F0-9]{24}$"),
+		("NT crypt",		"^\$3\$[a-zA-Z0-9./]{8}\$[a-zA-Z0-9./]{1,}$"),
+		("Skein-1024", 		"^[a-fA-F0-9]{256}$"),
+		(("RIPEMD-320", "RIPEMD-320(HMAC)"), 		"^[A-Fa-f0-9]{80}$"),
+		("Cisco IOS SHA256",	"^[a-zA-Z0-9]{43}$"),
+		("SHA-1(Django)", 	"^sha1\$.{0,32}\$[a-fA-F0-9]{40}$"),
+		("SHA-1 crypt",		"^\$4\$[a-zA-Z0-9./]{8}\$[a-zA-Z0-9./]{1,}$"),
+		("SHA-1(Hex)",		"^[a-fA-F0-9]{40}$"),
+		(("SHA-1(LDAP) Base64","Netscape LDAP SHA","NSLDAP"),	"^\{SHA\}[a-zA-Z0-9+/]{27}=$"),
+		("SHA-1(LDAP) Base64 + salt",	"^\{SSHA\}[a-zA-Z0-9+/]{28,}[=]{0,3}$"),
+		("SHA-512(Drupal)", 	"^\$S\$[a-zA-Z0-9\/\.]{52}$"),
+		("SHA-512 crypt",	"^\$6\$[a-zA-Z0-9./]{8}\$[a-zA-Z0-9./]{1,}$"),
+		("SHA-256(Django)", 	"^sha256\$.{0,32}\$[a-fA-F0-9]{64}$"),
+		("SHA-256 crypt",	"^\$5\$[a-zA-Z0-9./]{8}\$[a-zA-Z0-9./]{1,}$"),
+		("SHA-384(Django)", 	"^sha384\$.{0,32}\$[a-fA-F0-9]{96}$"),
+		("SHA-256(Unix)", 	"^\$5\$.{0,22}\$[a-zA-Z0-9\/\.]{43,69}$"),
+		("SHA-512(Unix)", 	"^\$6\$.{0,22}\$[a-zA-Z0-9\/\.]{86}$"),
+		(("SHA-384", "SHA3-384", "Skein-512(384)", "Skein-1024(384)"), 	"^[a-fA-F0-9]{96}$"),
+		(("SHA-512","SHA-512(HMAC)","SHA3-512","Whirlpool","SALSA-10","SALSA-20","Keccak-512","Skein-512","Skein-1024(512)"), 	"^[a-fA-F0-9]{128}$"),
+		("SSHA-1", 		"^({SSHA})?[a-zA-Z0-9\+\/]{32,38}?(==)?$"),
+		("MySQL 5.x", 		"^\*[a-f0-9]{40}$"),
+		(("MySQL 3.x", "DES(Oracle)", "LM", "VNC", "FNV-164"), 		"^[a-fA-F0-9]{16}$"),
+		("SAM(LM_Hash:NT_Hash)", 	"^[a-fA-F0-9]{32}:[a-fA-F0-9]{32}$"),
+		("MSSQL(2000)", 	"^0x0100[a-f0-9]{0,8}?[a-f0-9]{80}$"),
+		(("MSSQL(2005)", "MSSQL(2008)"), 	"^0x0100[a-f0-9]{0,8}?[a-f0-9]{40}$"),
+		(("substr(md5($pass),0,16)","substr(md5($pass),16,16)","substr(md5($pass),8,16)","CRC-64"),	"^[a-fA-F0-9./]{16}$"),
+		(("MySQL 4.x", "SHA-1", "HAVAL-160", "SHA-1(MaNGOS)", "SHA-1(MaNGOS2)", "TIGER-160", "RIPEMD-160", "RIPEMD-160(HMAC)", "TIGER-160(HMAC)", "Skein-256(160)", "Skein-512(160)"), "^[a-f0-9]{40}$"),
+		(("SHA-256","SHA-256(HMAC)","SHA-3(Keccak)","GOST R 34.11-94", "RIPEMD-256", "HAVAL-256", "Snefru-256","Snefru-256(HMAC)","RIPEMD-256(HMAC)", "Keccak-256", "Skein-256", "Skein-512(256)"), 	"^[a-fA-F0-9]{64}$"),
+		(("SHA-1(Oracle)","HAVAL-192", "Tiger-192", "TIGER-192(HMAC)"), "^[a-fA-F0-9]{48}$"),
+		(("SHA-224", "SHA-224(HMAC)", "HAVAL-224", "Keccak-224", "Skein-256(224)", "Skein-512(224)"), 	"^[a-fA-F0-9]{56}$"),
+		(("Adler32", "FNV-32","ELF-32","Joaat", "CRC-32", "CRC-32B", "GHash-32-3", "GHash-32-5","FCS-32","Fletcher-32","XOR-32"), 		"^[a-f0-9]{8}$"),
+		(("CRC-16-CCITT","CRC-16", "FCS-16"), 	"^[a-fA-F0-9]{4}$"),
+		(("MD5(HMAC(Wordpress))", "MD5(HMAC)", "MD5", "RIPEMD-128", "RIPEMD-128(HMAC)", "Tiger-128", "Tiger-128(HMAC)", "RAdmin v2.x", "NTLM", "Domain Cached Credentials(DCC)", "Domain Cached Credentials 2(DCC2)", "MD4", "MD2","MD4(HMAC)", "MD2(HMAC)", "Snefru-128", "Snefru-128(HMAC)", "HAVAL-128", "HAVAL-128(HMAC)", "Skein-256(128)", "Skein-512(128)","MSCASH2"), 	"^[0-9A-Fa-f]{32}$"), 	
 )
 
 # Function to identify all the hashes and return the results as list.
@@ -74,11 +90,11 @@ def identifyHashes(inputHash):
 	
 	# Loop through all the hashes in the HASHES tuple and find all the possible hashes.
 	for items in HASHES:
-		if (re.match(items[1],inputHash,re.IGNORECASE)):
+		if (re.match(items[1], inputHash, re.IGNORECASE)):
 			res += [items[0]] if (type(items[0]) is str) else items[0]	
 	return res
 
-#function Get input from the user maintaining the python compatibility with earlier and newer versions.
+# function Get input from the user maintaining the python compatibility with earlier and newer versions.
 def getInput(prompt):
 	if sys.hexversion > 0x03000000:
 		return input(prompt)
@@ -115,8 +131,8 @@ def startProcess():
 				print ("[+] " + results[0])
 				print ("[+] " + results[1])
 				print ("\nOther Possible Hash Algorithms found:\n")
-				for item in range(int(len(results))-2):
-					print ("[+] " + results[item+2])
+				for item in range(int(len(results)) - 2):
+					print ("[+] " + results[item + 2])
 			else:
 				print ("\nMost Probable Hash Algorithms found:\n")
 				for item in range(int(len(results))):
@@ -124,7 +140,7 @@ def startProcess():
 
 def main():
 
-	#Print the TITLE and USAGE and then start the main loop.
+	# Print the TITLE and USAGE and then start the main loop.
 	print (TITLE)
 	print (USAGE)
 	try:

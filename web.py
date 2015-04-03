09babb43a78e3ba29e83352fb9cdd14bf51e84cf
AnimeshShaw <psychocoder@outlook.com>
2015-04-03 20:11:35 +0530

    web.py updated and refactored

--- web.py
+++ web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-'''
-@about: Web service wrapper for hash identifcation.
+"""
+@about: Web service wrapper for hash identification.
 @author: moloch
 
 Usage: ./web.py or see --help for more options
@@ -43,7 +43,7 @@ hashes = {'hashes': [
 
 resp = requests.post("http://hashid.badwith.computer/", data=json.dumps(hashes))
 print resp.text
-'''
+"""
 
 import json
 import tornado.ioloop
@@ -52,14 +52,21 @@ import tornado.log
 
 from tornado.gen import coroutine, Return
 from tornado.options import define, options
-from HashIdentifier import identify_hashes
+from hashidentifier.HashIdentifier import identify_hashes
 
 
 class MainHandler(tornado.web.RequestHandler):
+    """
+    Creates a simple JSON API
+    """
 
-    ''' Creates a simple JSON API '''
+    def data_received(self, chunk):
+        pass
 
     def initialize(self):
+        """
+        Add headers.
+        """
         self.set_header("Server", "HashIdentifier")
         self.set_header("X-Content-Type-Options", "nosniff")
         self.set_header("Access-Control-Allow-Origin", "*")
@@ -67,18 +74,24 @@ class MainHandler(tornado.web.RequestHandler):
 
     @coroutine
     def identify_hashes(self, hsh):
-        ''' Just a small async wrapper function '''
+        """
+        Just a small async wrapper function
+        """
         raise Return(identify_hashes(hsh))
 
     @coroutine
     def get(self, *args):
-        ''' GET idnetifies a single hash '''
+        """
+        GET identifies a single hash
+        """
         results = yield self.identify_hashes(args[0])
         self.write({args[0]: results})
 
     @coroutine
     def post(self, *args):
-        ''' POST can accept multiple hashes '''
+        """
+        POST can accept multiple hashes
+        """
         response = {}
         try:
             hashes = json.loads(self.request.body)['hashes']
@@ -88,10 +101,7 @@ class MainHandler(tornado.web.RequestHandler):
             response = {'error': 'could not parse request'}
         self.write(response)
 
-
-application = tornado.web.Application([
-    (r"/(.*)", MainHandler),
-])
+application = tornado.web.Application([(r"/(.*)", MainHandler), ])
 
 define("port",
        default="8888",

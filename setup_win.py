5eb735374c226654a40eea0c74493e26080b8529
Psycho_Coder <AnimeshShaw@users.noreply.github.com>
2015-04-03 20:48:40 +0530

    Delete setup_win.py

--- setup_win.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-try:
-    import py2exe
-except ImportError:
-    print("Python 3.x is required to create Windows binaries.")
-
-setup(console=['hashidentifier/HashIdentifier.py'])
